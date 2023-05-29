5bdaa70caf9015b11a40ed2460c383ac0074efeb
constantinos <connoskats@gmail.com>
2023-05-29 17:39:30 +0100

    Code style fixes based on PEP8.

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -8,22 +8,25 @@ from catchment import models, views
 
 def main(args):
     """The MVC Controller of the environmental data system.
-
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    InFiles = args.infiles
+    infiles = args.infiles
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
-    
-    
+       
     for filename in InFiles:
         measurement_data = models.read_variable_from_csv(filename)
-        
-        view_data = {'daily sum': models.daily_total(measurement_data), 'daily average': models.daily_mean(measurement_data), 'daily max': models.daily_max(measurement_data), 'daily min': models.daily_min(measurement_data)}
+        view_data = {
+            'daily sum': models.daily_total(measurement_data),
+            'daily average': models.daily_mean(measurement_data), 
+            'daily max': models.daily_max(measurement_data), 
+            'daily min': models.daily_min(measurement_data)
+        }
         
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
